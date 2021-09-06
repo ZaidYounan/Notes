24c54051c2ecc34fde6e18613664e43d9d7fe44e
ZaidYounan <zaid.younan@gmail.com>
2021-09-06 23:19:04 +1000

    Update with new algorithm attempts

--- Algorithms/index.js
+++ Algorithms/index.js
@@ -15,6 +15,7 @@ var twoSum = function(numbers, target) {
 };
 
 
+
 /* Two Sum version II, first successfull attempt - VERY SLOW.
 Array is ordered, thus there must be a faster way to eliminate 
 redudant numbers */
@@ -32,6 +33,8 @@ var twoSum = function(numbers, target) {
     }
     return indexPair;
 };
+
+
 
 
 /* Two Sum II second successful attempt, much faster. This one was tricky. 
@@ -70,3 +73,78 @@ var twoSum = function(numbers, target) {
 
     return indexPair;
 };
+
+
+
+
+/* First (failed) attempt at Zig Zag Sequence */
+
+function findZigZagSequence(a, n) {
+    a.sort(function(x, y) {return x - y});
+   
+    var mid = Math.floor((n + 1) / 2);
+    
+    for (let i = 0; i < n; i++) {
+      if ( a[i] >= a[mid] ) {
+          a.push(i);
+        } else if (a[i] < a[mid]) {
+          a.unshift(i);
+        }
+      }
+ 
+    return a;
+}
+
+console.log(findZigZagSequence([1, 2, 3, 4, 5, 6, 7], 7));
+
+
+
+/* First successful attempt, but has a slow run-time. Next attempt is much faster. */
+
+
+function findMaxSum(numbers) {
+    sortedArray = numbers.sort(function(a, b){return a - b});
+    const largest = sortedArray.length - 1
+    const secondLargest = largest - 1
+    
+    return sortedArray[largest] + sortedArray[secondLargest];
+}
+  
+  
+  /* If the array is sorted, then you can just grab the 
+  last two indexes and sum them together to get the largest sum. 
+  */
+
+console.log(findMaxSum([ 5, 9, 7, 11 ])); // should print 20
+
+
+
+
+
+/* Go through the array and iterativelystore the highest / second 
+highest values, then sum together.
+
+
+If, instead, the array was already sorted - then you could just grab the 
+last two indexes and sum them together to get the largest sum. 
+*/
+
+function findMaxSum(numbers) {
+    let highestInt = 0;
+    let nextHighestInt = 0;
+    
+    for (let i = 0; i < numbers.length; i++) {
+        
+        if (numbers[i] > highestInt) {
+        nextHighestInt = highestInt;
+        highestInt = numbers[i];
+        } else if (numbers[i] > nextHighestInt) {
+        nextHighestInt = numbers[i];
+        } else {
+        continue;
+        }
+    }
+    return highestInt + nextHighestInt;
+}
+
+console.log(findMaxSum([ 5, 9, 7, 11 ])); // should print 20
